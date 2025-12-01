a172a6926c0e994ccc64c15e98dd5ab5d2a092b4
FerizatJussupbekova ferizat12@gmail.com <ferizat12@gmail.com>
2025-12-01 16:29:45 +0100

    added devis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.action_mailer.default_url_options = { host: 'https://rails-daytrip-f7598d0eeaf7.herokuapp.com/'}
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
