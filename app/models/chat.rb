b584d15ebcd54ec9ac2170efedfe94ada3949a66
James Iles <jamesadiles@gmail.com>
2025-12-04 16:49:31 +0000

    Styling - colors, fonts and layouts - adjusted

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,7 +3,7 @@ class Chat < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
 
-  DEFAULT_TITLE = "Untitled"
+  DEFAULT_TITLE = "Adventure awaits..."
   TITLE_PROMPT = <<~PROMPT
     Generate a short, descriptive, 3-to-6-word title that summarizes the user question for a chat conversation.
     PROMPT
